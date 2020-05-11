6046c0cdc9b8b1a79e4400fb8010308afabf217f
Harry Alto <harry.alto@gmail.com>
2020-05-11 03:39:20 -0700

    New Files

---
+++ src/test/java/com/hnath/spring/learning/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnath.spring.learning.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
